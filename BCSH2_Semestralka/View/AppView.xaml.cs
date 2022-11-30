a155fa306db9fe75ca8cebf84b6c37f4a11d511a
JoseTheChamp <apep.knotek@gmail.com>
2022-11-30 10:55:43 +0100

    Even smaller changes

--- BCSH2_Semestralka/View/AppView.xaml.cs
+++ BCSH2_Semestralka/View/AppView.xaml.cs
@@ -43,28 +43,5 @@ namespace BCSH2_Semestralka.View
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
-
-        void DataWindow_Closing(object sender, CancelEventArgs e)
-        {
-
-            /*
-            MessageBox.Show("Closing called");
-            // If data is dirty, notify user and ask for a response
-            if (false)
-            {
-                string msg = "Data is dirty. Close without saving?";
-                MessageBoxResult result =
-                  MessageBox.Show(
-                    msg,
-                    "Data App",
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Warning);
-                if (result == MessageBoxResult.No)
-                {
-                    // If user doesn't want to close, cancel closure
-                    e.Cancel = true;
-                }
-            }*/
-        }
     }
 }
